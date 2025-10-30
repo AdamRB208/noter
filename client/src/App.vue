72d55cb65eb660708cb356592df5134c6fdf609f
AdamRB208 <adamrborup@gmail.com>
2025-10-29 21:40:27 -0600

    added background image

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,5 +18,10 @@ import { RouterView } from 'vue-router';
 
 <style lang="scss">
 
+main {
+  background-image: url(https://plus.unsplash.com/premium_photo-1726244024984-df9cce583191?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFwZXIlMjBiYWNrZ3JvdW5kfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=500);
+  width: 100%;
+  height: 100%;
+}
 
 </style>
