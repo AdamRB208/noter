4ffdd407061e56f78221ad090cf199e62e504430
AdamRB208 <adamrborup@gmail.com>
2025-11-05 14:08:09 -0700

    displaying active notebook pojo.

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,22 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import Offcanvas from '@/components/Offcanvas.vue';
+import { computed, watch } from 'vue';
 
+const activeNotebook = computed(() => AppState.activeNotebook)
+
+watch(activeNotebook, (newNotebook) => {
+  if (newNotebook?.id) {
+    return activeNotebook
+  }
+})
 
 </script>
 
 <template>
   <div>
     <Offcanvas />
-
+    <div>{{ activeNotebook }}</div>
   </div>
 </template>
 
