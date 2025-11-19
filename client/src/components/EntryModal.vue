efcb0bb77570de0bc1e3172a5422af1ef80e5828
AdamRB208 <adamrborup@gmail.com>
2025-11-19 13:34:44 -0700

    created image preview for form.

--- client/src/components/EntryModal.vue
+++ client/src/components/EntryModal.vue
@@ -1,39 +1,49 @@
 <script setup>
+import { ref } from 'vue';
+
+
+
+const formData = ref({
+  description: '',
+  img: '',
+})
 
 </script>
 
 
 <template>
   <div class="modal" tabindex="-1" id="entryModal" aria-labelledby="entryModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="entryModal" aria-labelledby="entryModal">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-    <div class="mb-3">
-      <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-    <div class="mb-3">
-      <label for="imageUpload" class="form-label">Upload Image</label>
-      <input>
-    </div>
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1">
-    </div>
-  </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="entryModal" aria-labelledby="entryModal">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="mb-3">
+              <label for="description" class="form-label">Example textarea</label>
+              <textarea v-model="formData.description" class="form-control" id="description" rows="3" required
+                maxlength="2000"></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="img" class="form-label">Upload Image</label>
+              <input v-model="formData.img" class="form-control" type="url" required id="img" name="image"
+                maxlength="500">
+            </div>
+            <div class="mb-3">
+              <label for="img" class="form-label">Image Preview</label>
+              <img class="imagePreview" :src="formData.img" :alt="`preview image of ${formData.img}`">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 
